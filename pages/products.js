0119c9c4486c90d7285fc9ab8f10c59f3d9bbb54
AbdeldjabarDev <ousseddikabdo@gmail.com>
2022-12-09 10:09:12 +0100

    Fixed import issue

--- pages/products.js
+++ pages/products.js
@@ -25,7 +25,7 @@ export default function Products(props)
 </div>
 <div className="w-full h-[400px] bg-[#002c3e] flex mt-20 ">
     <div className="flex flex-row w-fit mr-auto ml-72 mt-20">
-<ul className="w-60 h-fit mr-20 border border-white">
+<ul className="w-60 h-fit mr-20 ">
 
 <li className="font-semibold text-white text-3xl mb-10"><p>Reach at ...</p></li>
 <li className="mb-5 hover:text-red-500 text-white"><a className="flex flex-row gap-3"><img className="color-white" src="/images/location-svgrepo-com.svg" width="20" height="20s" ></img><p>location</p></a></li>
@@ -39,11 +39,11 @@ export default function Products(props)
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, aliquid qui. Ipsa sit, quibusdam tempora nostrum molestias, consequatur laudantium reprehenderit mollitia explicabo porro similique ea accusantium quos assumenda dolor nemo.
 </li>
 <li><ul className="flex flex-row gap-8 mt-6  w-fit">
-    <li><a><img className="border border-orange-500 cursor-pointer" src="/images/facebook-svgrepo-com.svg"  alt="social media" width="65" height="65"></img></a></li>
-    <li><a><img className="border border-orange-500 cursor-pointer" src="/images/instagram-svgrepo-com.svg" alt="social media" width="65" height="65"></img></a></li>
-    <li><a><img className="border border-orange-500 cursor-pointer" src="/images/twitter-svgrepo-com.svg"   alt="social media" width="65" height="65"></img></a></li>
-    <li><a><img className="border border-orange-500 cursor-pointer" src="/images/linkedin-svgrepo-com.svg"  alt="social media" width="65" height="65"></img></a></li>
-    <li><a><img className="border border-orange-500 cursor-pointer" src="/images/pinterest-svgrepo-com.svg" alt="social media" width="65" height="65"></img></a></li>
+    <li><a><img className="cursor-pointer" src="/images/facebook-svgrepo-com.svg"  alt="social media" width="65" height="65"></img></a></li>
+    <li><a><img className="cursor-pointer" src="/images/instagram-svgrepo-com.svg" alt="social media" width="65" height="65"></img></a></li>
+    <li><a><img className="cursor-pointer" src="/images/twitter-svgrepo-com.svg"   alt="social media" width="65" height="65"></img></a></li>
+    <li><a><img className="cursor-pointer" src="/images/linkedin-svgrepo-com.svg"  alt="social media" width="65" height="65"></img></a></li>
+    <li><a><img className="cursor-pointer" src="/images/pinterest-svgrepo-com.svg" alt="social media" width="65" height="65"></img></a></li>
 
     </ul></li>
 </ul>
